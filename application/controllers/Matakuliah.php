da92531b66ef257cd07612804c4ab3c57bb144ba
unknown <akhmadzamroni138@gmail.com>
2020-10-10 03:56:59 +0700

    wp2

---
+++ application/controllers/Matakuliah.php
@@ -0,0 +1,15 @@
+<?php 
+
+/**
+ * 
+ */
+class Matakuliah extends CI_Controller
+{
+	
+	public function index()
+	{
+		$this->load->view('formmatakuliah');
+	}
+}
+
+
---
+++ application/controllers/perkenalan.php
@@ -0,0 +1,11 @@
+<?php
+class perkenalan extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1> Perkenalan </h1>";
+        echo "Nama saya Akhmad Samroni<br>
+              Saya tinggal di Slawi <br>";
+
+    }
+}
